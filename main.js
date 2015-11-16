a132207e9e45c20539d1d3c69c0a61f6d46da157
eparkhurst <eli.parkhurst@gmail.com>
2015-11-16 12:44:06 -0700

    added click logic, doesn't work

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ $.get('kimonoData.json', function(data){
     var sendee = movie.replace(/\s/g,"+");
     $.get('http://www.omdbapi.com/?t='+sendee+'&y=&plot=short&r=json', function(omdb){
       $(".container").append(omdb.Title);
-      $(".container").append('<img src='+omdb.Poster+'>')
+      $(".container").append('<img class="choice1" src='+omdb.Poster+'>')
       console.log(omdb)
       var rating1 = omdb.imdbRating;
         //*******
@@ -17,9 +17,8 @@ $.get('kimonoData.json', function(data){
       var sendee2 = movie2.replace(/\s/g,"+");
       $.get('http://www.omdbapi.com/?t='+sendee2+'&y=&plot=short&r=json', function(omdb){
         $(".container").append(omdb.Title);
-        $(".container").append('<img src='+omdb.Poster+'>')
+        $(".container").append('<img class ="choice2" src='+omdb.Poster+'>')
         var rating2 = omdb.imdbRating;
-
         var correct = 0;
         if(rating1>rating2){
         correct = 1;
@@ -27,6 +26,18 @@ $.get('kimonoData.json', function(data){
       })
     })
 })
+
+// var userChoice = 0;
+// $('.choice1').click(function(){
+//   userChoice = 1;
+//   console.log('choice 1')
+// })
+// $('.choice2').click(function(){
+//   userChoice = 2;
+//   console.log('choice 2')
+// })
+
+
 
 var $randNum =function(){
   return Math.floor(Math.random()*1000);
