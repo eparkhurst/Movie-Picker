0f9fc5ea7125ae1ec9153b468851dc907a58bd1f
eparkhurst <eli.parkhurst@gmail.com>
2015-11-16 12:33:26 -0700

    checks which imdb rating is higher. No user functionality

--- main.js
+++ main.js
@@ -4,27 +4,28 @@ console.log("sanity check");
 
 
 $.get('kimonoData.json', function(data){
-  var $rating1 =1
-  var $rating2 =1
-  for (var i = 0; i < 2; i++) {
+
     var movie = data.results.collection1[$randNum()].property1.text;
     var sendee = movie.replace(/\s/g,"+");
     $.get('http://www.omdbapi.com/?t='+sendee+'&y=&plot=short&r=json', function(omdb){
-      if(i === 0){
-        $rating1 = omdb.imdbRating;
-        console.log($rating1)
-      }else{
-        $rating2 = omdb.imdbRating;
-        console.log($rating2)
-      }
       $(".container").append(omdb.Title);
       $(".container").append('<img src='+omdb.Poster+'>')
+      console.log(omdb)
+      var rating1 = omdb.imdbRating;
+        //*******
+      var movie2 = data.results.collection1[$randNum()].property1.text;
+      var sendee2 = movie2.replace(/\s/g,"+");
+      $.get('http://www.omdbapi.com/?t='+sendee2+'&y=&plot=short&r=json', function(omdb){
+        $(".container").append(omdb.Title);
+        $(".container").append('<img src='+omdb.Poster+'>')
+        var rating2 = omdb.imdbRating;
+
+        var correct = 0;
+        if(rating1>rating2){
+        correct = 1;
+        }else correct = 2;
+      })
     })
-  }
-  console.log($rating1+" , "+$rating2 );
-  if($rating1 > $rating2){
-    console.log('1wins')
-  }else console.log('2wins')
 })
 
 var $randNum =function(){
