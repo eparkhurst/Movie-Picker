51a5b6018429def164f3057ab9436b3bdcf44b6e
eparkhurst <eli.parkhurst@gmail.com>
2015-11-17 13:22:59 -0700

    user names can be input and are stored in local storage. the scores are also stored to a users name

--- main.js
+++ main.js
@@ -3,6 +3,14 @@ console.log("sanity check");
 var $score =0;
 var $correct = 0;
 var $choiceCount = 0;
+var currentUser = "";
+var userList = [];
+
+for ( var i = 0, len = localStorage.length; i < len; ++i ) {
+  userList.push( JSON.parse(localStorage.getItem( localStorage.key( i ) )).user );
+}
+
+
 
 run();
 function run(){
@@ -14,7 +22,7 @@ function run(){
         $(".leftChoice").append('<img class="choice1" src='+omdb.Poster+'>')
         //console.log(omdb)
         var rating1 = omdb.imdbRating;
-        console.log("1 is "+rating1);
+      //  console.log("1 is "+rating1);
           //*******
         var movie2 = data.results.collection1[$randNum()].property1.text;
         var sendee2 = movie2.replace(/\s/g,"+");
@@ -22,7 +30,7 @@ function run(){
           $(".rightChoice").append("<h1>"+omdb.Title+"</h1>");
           $(".rightChoice").append('<img class ="choice2" src='+omdb.Poster+'>')
           var rating2 = omdb.imdbRating;
-          console.log('2 is '+rating2);
+        //  console.log('2 is '+rating2);
           if(rating1 === rating2){
             $choiceCount -=1;
             reset();
@@ -61,12 +69,13 @@ var userChoice = 0;
 }
 
 function reset(){
-  if($choiceCount == 10){
+  if($choiceCount == 10){                   //When a game is finished
     alert("Your score is "+$score+"/10");
+    storeScore();
     $score = 0;
     $choiceCount = 0;
   }
-  console.log($score);
+  //console.log($score);
   $('.leftChoice').empty();
   $('.rightChoice').empty();
   run();
@@ -74,4 +83,23 @@ function reset(){
 
 var $randNum =function(){
   return Math.floor(Math.random()*1000);
+}
+
+$(".submit").click(function(){
+  $(".overlay").css("display","none");
+  $(".inputBox").css("display", "none");
+  currentUser = $(".userName").val().toLowerCase();
+  if($.inArray(currentUser, userList) === -1){
+    var newObj = {
+    'user' : currentUser,
+    'history' : []
+    }
+    localStorage.setItem(currentUser, JSON.stringify(newObj))
+  }
+})
+
+function storeScore(){                                     //Stores user scores
+  var userObj = JSON.parse(localStorage.getItem(currentUser))
+  userObj.history.push($score)
+  localStorage.setItem(currentUser, JSON.stringify(userObj))
 }
